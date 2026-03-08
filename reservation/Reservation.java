3ddc08105cb3a2aa5eb13741f4f66bc35a54bd1c
= <=>
2026-03-09 00:58:13 +0530

    Update to hold Reservation IDs

--- reservation/Reservation.java
+++ reservation/Reservation.java
@@ -3,6 +3,8 @@ package com.seveneleven.bookmystayapp.reservation;
 public class Reservation {
     private String guestName;
     private String roomType;
+    private String reservationId; 
+    private String status; 
     
     /**
      * Constructor to instantiate a new reservation
@@ -13,6 +15,7 @@ public class Reservation {
     public Reservation(String guestName, String roomType) {
         this.guestName = guestName;
         this.roomType = roomType;
+        this.status = "PENDING";
     }
     
     /**
@@ -34,12 +37,45 @@ public class Reservation {
     }
     
     /**
+     * Method to get the ID of the reservation
+     * 
+     * @return The ID of the reservation
+     */
+    public String getReservationId() { 
+    	return reservationId; 
+    }
+    
+    /**
+     * Method to get the status of the current reservation
+     * 
+     * @return The status of the current reservation
+     */
+    public String getStatus() { return status; }
+    
+    /**
+     * Method to set the reservation ID of the reservation
+     * 
+     * @param reservationId	The reservation ID to set
+     */
+    public void setReservationId(String reservationId) { 
+    	this.reservationId = reservationId; 
+    }
+    
+    /**
+     * Method to set the status of the current Reservation
+     * 
+     * @param status	The status to set	
+     */
+    public void setStatus(String status) { this.status = status; }
+    
+    /**
      * Method to get a String representation of the reservation
      * 
      * @return String representation of the reservation
      */
     @Override
     public String toString() {
-        return "Reservation [Guest: " + guestName + ", Room Type: " + roomType + "]";
+        return String.format("Reservation [ID: %s | Guest: %s | Room: %s | Status: %s]", 
+                (reservationId != null ? reservationId : "N/A"), guestName, roomType, status);
     }
 }
