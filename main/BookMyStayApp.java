b78be2bf979fc7c867b6a0a2128f987a4b3d8c60
Azaken <127621783+Azaken1248@users.noreply.github.com>
2026-03-09 01:00:07 +0530

    Update version number to 5.0 in BookMyStayApp

--- main/BookMyStayApp.java
+++ main/BookMyStayApp.java
@@ -14,7 +14,7 @@ import com.seveneleven.bookmystayapp.room.services.SearchService;
  * Main Entry point of the BookMyStayApp 
  * 
  * @author Developer
- * @version 4.0
+ * @version 5.0
  */
 public class BookMyStayApp {
 
